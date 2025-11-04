9549f8b3e4c7e574adc702caecc05cfec97f0387
Samuel Resende <samuelres444@gmail.com>
2025-11-04 13:04:55 -0300

    feat: Estrutura base Spring Boot e dependencias essenciais

---
+++ src/main/java/com/samuel_resende/controle_funcionario_EPI/ControleFuncionarioEpiApplication.java
@@ -0,0 +1,13 @@
+package com.samuel_resende.controle_funcionario_EPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFuncionarioEpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFuncionarioEpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuel_resende/controle_funcionario_EPI/ControleFuncionarioEpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel_resende.controle_funcionario_EPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFuncionarioEpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
