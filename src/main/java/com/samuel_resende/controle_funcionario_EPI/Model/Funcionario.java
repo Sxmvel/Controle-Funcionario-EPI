3019e80a3061009d5b357d8a7f7822b49eb5d774
Samuel Resende <samuelres444@gmail.com>
2025-11-04 14:15:08 -0300

    Cria a classe model funcionario e seus atributos

--- src/main/java/com/samuel_resende/controle_funcionario_EPI/Model/Funcionario.java
+++ src/main/java/com/samuel_resende/controle_funcionario_EPI/Model/Funcionario.java
@@ -1,5 +1,48 @@
 package com.samuel_resende.controle_funcionario_EPI.Model;
 
+import  java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data // Lombok para gerar getter e setter
+
+@Entity // Anotacao JPA para mapear a classe como entidade
+
+@Table(name = "Funcionario")
+
+
 public class Funcionario {
-    
+
+    // atributos da entidade funcionario.
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id_funcionario;
+
+    // Coluna para o nome do funcionário
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    // Coluna para o CPF (Unique e Not Null)
+    @Column(nullable = false, length = 14, unique = true)
+    private String cpf;
+
+    // Coluna para a Matrícula (Unique e Not Null)
+    @Column(nullable = false, length = 20, unique = true)
+    private String matricula;
+
+    // Coluna para a Função/Cargo
+    @Column(nullable = false, length = 50)
+    private String funcao;
+
+    // Coluna para a Data de Contratação
+    @Column(name = "data_contratacao")
+    private LocalDate dataContratacao; // Usamos LocalDate para mapear o tipo DATE do SQL
+   
 }
