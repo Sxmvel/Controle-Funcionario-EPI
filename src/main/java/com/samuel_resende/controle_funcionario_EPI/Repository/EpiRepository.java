7f632add239f92e4f93f279802815bf072b595b6
Samuel Resende <samuelres444@gmail.com>
2025-11-05 15:09:44 -0300

    Implementa EpiRepository

--- src/main/java/com/samuel_resende/controle_funcionario_EPI/Repository/EpiRepository.java
+++ src/main/java/com/samuel_resende/controle_funcionario_EPI/Repository/EpiRepository.java
@@ -1,5 +1,14 @@
 package com.samuel_resende.controle_funcionario_EPI.Repository;
 
-public class EpiRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.samuel_resende.controle_funcionario_EPI.Model.EPI;
+
+
+@Repository
+public interface EpiRepository extends JpaRepository<EPI, Long> {
+    
+
     
 }
